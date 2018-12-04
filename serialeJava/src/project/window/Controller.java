e5676b2e4560eb978f294a2be5e0b14f3e47e670
Mariusz Antas <dywanoid@wp.pl>
2018-12-04 10:51:15 +0100

    Controller with sample functions added

--- serialeJava/src/project/window/Controller.java
+++ serialeJava/src/project/window/Controller.java
@@ -1,4 +1,23 @@
 package project.window;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+    public Controller() {
+    }
+
+    @FXML
+    private void clickXD() {
+        System.out.println("Wcisnales XD");
+    }
+
+    @FXML
+    private void clickXDD() {
+        System.out.println("Wcisnales XDD");
+    }
+    @FXML
+    private void clickXDDD() {
+        System.out.println("Wcisnales XDDD");
+    }
+
 }
